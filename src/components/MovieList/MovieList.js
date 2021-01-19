0b09eff5af26a16e046e4068970888c964fb0f69
Brandon Bernal <bernal.brandon@gmail.com>
2021-01-19 05:32:14 -0500

    Fix movie title stretching parent container

--- src/components/MovieList/MovieList.js
+++ src/components/MovieList/MovieList.js
@@ -5,22 +5,15 @@ const MovieList = ({movies, handleNominateClick, NominateComponent, nominations}
   return (
     <>
       {movies.map((movie) =>
-        <div
-          key={movie.imdbID}
-          className="movie m-2"
-        >
+        <div key={movie.imdbID} className="movie m-2">
           <Image 
             src={movie.Poster} 
             alt='movie' 
             className="poster"
           />
-          <div
-            className="d-flex flex-column justify-content-between m-2"
-          >
-            <h6
-              className="movie-title"
-            >
-            {movie.Title} ({movie.Year})
+          <div className="d-flex flex-column justify-content-between m-2">
+            <h6 className="movie-title">
+              {movie.Title} ({movie.Year})
             </h6>
             <NominateComponent
               movie={movie}
